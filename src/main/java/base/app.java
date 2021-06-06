3c42e45876707d48b9692bba7db3e6f175280f3d
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-06 16:55:02 -0400

    Initial commit

---
+++ src/main/java/base/app.java
@@ -0,0 +1,29 @@
+package base;
+import java.util.*;
+
+public class app {
+
+    public static void main(String[] args) {
+        Scanner newScan = new Scanner(System.in);
+
+        //Input
+        System.out.print("Enter a noun: ");
+        String noun = newScan.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = newScan.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adj = newScan.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adv = newScan.nextLine();
+
+        //Concat
+        String output = "Do you " + verb + " your " + adj + " " + noun + " " + adv + "? That's hilarious!";
+
+        //Output
+        System.out.println(output);
+    }
+
+}
